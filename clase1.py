e2217899b49973b2831198e4b7a475b1436b84da
Brian Zuñiga Uribe <brian-kio@hotmail.com>
2020-09-16 21:41:43 -0500

    Clase numero 2

--- clase1.py
+++ clase1.py
@@ -1,12 +1,29 @@
 #Parametros
 #Variables que se les asigna un nuevo nombre
 #Y tiene un orden de ser llenados
+
+# Enteros
+# Flotantes 
+# Cadenas
+# booleanas
+a = 5.14
+a = 12
+a = 5
+b = a+5
+c = "HOLA MUNDO"
+d = c+" NICK"
+d = True
+e = c+d
+# b = 10
+
+
 def operaciones(a,b):
     return a**b
 
 def quitaChar(param1,ch1):
     #lee de teclado
     return param1
+    
 p = operaciones(2,6)
 print(p)
 param1 = int(input("Introduce param1 "))
---
+++ clase2.py
@@ -0,0 +1,68 @@
+a = 20
+b = -5
+c = 1
+d = 2
+# a == 20 or b == 20 -> True or False ->C1
+# c == 1 or d == 2 -> True or False->C2
+# C1 and C2
+# and or & ||
+# != 
+if (a == 20 or b == 20) and (c == 1 or d == 2):
+    print("A vale: ",a)
+elif a == 19:
+    print("A no vale 10")
+elif a==18:
+    print("cualquier cosa 1")
+elif a==17:
+    print("Cualquier cosa 2")
+else:
+    print("Cualquier cosa 3")
+# x < 10
+# 
+for x in range(10):
+    print(x)
+print("\n")
+for x in range(5,10):
+    print(x)
+print("\n")
+cadena = "Brian Zuniga Uribe"
+cadenas = ["hola","mundo","nick","madworld"]
+primes = [2, 3, 5, 7]
+a = "nick"
+b = "levi"
+c = a+b
+## output
+## c = nicklevi
+ans = ""
+for x in cadena:
+    if x != "a":
+        ans = ans+x
+    # x = Z
+    # ans = "Brian "
+    # ans = ans+x
+    # ans = "Brian Z"
+#param1 será una cadena
+#ch1 será el caracter que debemos eliminar de la cadena param1
+
+def quitaChar(param1, ch1):
+    ans = ""
+    for x in param1:
+        if x != ch1:
+            ans = x+ans
+    return ans
+
+def volteaCad(param1):
+    ans = ""
+    return ans
+
+
+#inpt = input("Ingresa una cadena: ")
+#char = input("Ingresa el char a eliminar: ")
+#respuesta = quitaChar(inpt,char)
+##print(respuesta)
+print("\n")
+
+for cadena in cadenas:
+    print(cadena)
+    for char in cadena:
+        print(char)
