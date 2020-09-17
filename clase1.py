6070b1c707157db1eb6e1e32bcc3f952b9f2f4e1
Brian Zuñiga Uribe <brian-kio@hotmail.com>
2020-09-16 19:54:34 -0500

    Primer clase

---
+++ clase1.py
@@ -0,0 +1,19 @@
+#Parametros
+#Variables que se les asigna un nuevo nombre
+#Y tiene un orden de ser llenados
+def operaciones(a,b):
+    return a**b
+
+def quitaChar(param1,ch1):
+    #lee de teclado
+    return param1
+p = operaciones(2,6)
+print(p)
+param1 = int(input("Introduce param1 "))
+param2 = int(input("Introduce param2 "))
+d = operaciones(param1,param2)
+
+print(d)
+# 100
+# 011
+# 111
